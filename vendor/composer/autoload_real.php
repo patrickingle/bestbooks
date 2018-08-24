813920ccb8f4146029e50a9941fef177206689c6
PHK Corporation <phkcorporation@gmail.com>
2018-08-24 06:59:04 -0400

    added koolphp for reporting/report generation

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -40,6 +40,20 @@ class ComposerAutoloaderInit54c94ca84c443acedd8bc0f575abd635
 
         $loader->register(true);
 
+        $includeFiles = require __DIR__ . '/autoload_files.php';
+        foreach ($includeFiles as $fileIdentifier => $file) {
+            composerRequire54c94ca84c443acedd8bc0f575abd635($fileIdentifier, $file);
+        }
+
         return $loader;
+    }
+}
+
+function composerRequire54c94ca84c443acedd8bc0f575abd635($fileIdentifier, $file)
+{
+    if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
+        require $file;
+
+        $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
     }
 }
